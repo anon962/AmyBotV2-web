e85fdaf48e12ad36f2a9d77be78044d9c6572043
nope <nope@gisadan.dev>
2024-04-05 06:06:13 -0400

    humanize price

--- src/lib/equip-table.svelte
+++ src/lib/equip-table.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+    import { sort } from 'radash'
     import type { Equip } from './equip'
 
     export let data: Equip[]
 
+    const dataSorted = sort(data, eq => eq.price, true)
+
     const name = data[0].name
+
+    function humanizePrice(val: number, precision=1): string {
+        let [factor, unit] = [1, '']
+
+        if (val >= 1000 ** 2) {
+            factor = 1000 ** 2
+            unit = 'm'
+        } else if (val >= 1000 ** 1) {
+            factor = 1000 ** 1
+            unit = 'k'
+        } else {
+            factor = 1
+            unit = "c"
+        }
+
+        let afterDiv = (val / factor).toFixed(precision)
+
+        // Only show trailing zeros if price is in the mils
+        //   eg "26.0m" is okay but "26.0k" is ugly
+        if (afterDiv.endsWith(".0") && unit != "m") {
+            afterDiv = afterDiv.replace(".0", "")
+        }
+
+        return `${afterDiv}${unit}`
+    }
 </script>
 
-<div class="collapse collapse-plus bg-base-200 max-w-[75rem]">
+<div class="collapse collapse-plus bg-base-200 max-w-[65rem]">
     <input type="checkbox" {name} />
 
     <h1 class="collapse-title flex gap-3">
@@ -19,7 +47,7 @@
             <table class="table table-zebra table-sm md:table-md table-pin-rows">
                 <thead>
                     <tr class="bg-base-200">
-                        <td>Price</td>
+                        <td class="text-end max-w-content">Price</td>
                         <td>Stats</td>
                         <td>Level</td>
                         <td>Auction / Date</td>
@@ -29,9 +57,9 @@
                 </thead>
 
                 <tbody>
-                    {#each data as eq}
+                    {#each dataSorted as eq}
                         <tr>
-                            <td>{eq.price}</td>
+                            <td class="text-end max-w-content">{humanizePrice(eq.price)}</td>
                             <td class="min-w-content whitespace-pre">{eq.stats.join('\n')}</td>
                             <td>{eq.level}</td>
                             <td>{eq.auction.end_time}</td>
