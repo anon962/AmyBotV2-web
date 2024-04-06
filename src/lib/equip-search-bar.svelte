068cdad193f46a6c0cd52368530891a678a3387e
nope <nope@gisadan.dev>
2024-04-06 17:19:36 -0400

    add filter dialog button

--- src/lib/equip-search-bar.svelte
+++ src/lib/equip-search-bar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { formToParams, setEquipFormContext } from './equip-form-context/context'
     import { getEquipSearchContext } from './equip-search-context'
+    import FilterIcon from './icons/filter-icon.svelte'
 
     const { params, setParams } = getEquipSearchContext()
     const { form, register } = setEquipFormContext($params)
@@ -14,13 +15,21 @@
 
 <!-- @todo: add other filters inside dialog -->
 <form class="w-full max-w-[50rem]" on:submit|preventDefault={handleSubmit}>
-    <input
-        use:register={'name'}
-        class="input input-bordered input-primary w-full"
-        name="name"
-        type="text"
-        placeholder="peerl heimd oak"
-    />
+    <label class="w-full px-0 input input-bordered input-primary flex gap-2 items-center">
+        <input
+            use:register={'name'}
+            class="pl-4 grow"
+            name="name"
+            type="text"
+            placeholder="peerl heimd oak"
+        />
+
+        <div class="p-1 h-full">
+            <button on:click|preventDefault type="button" class="btn btn-sm btn-ghost h-full">
+                <FilterIcon />
+            </button>
+        </div>
+    </label>
 </form>
 
 <style lang="postcss">
