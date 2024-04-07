25ae475c20c8d8b8eb7af84dac441693d66ede3c
nope <nope@gisadan.dev>
2024-04-07 06:45:49 -0400

    tidy up

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -8,18 +8,28 @@
     $: iconVariant = $isDarkTheme ? ('dark' as const) : ('light' as const)
 </script>
 
-<footer class="mt-auto footer p-6 bg-base-200 text-base-content rounded flex">
+<footer class="mt-auto footer p-6 bg-base-200 text-base-content rounded flex items-center">
     <ThemePicker />
 
     <nav class="w-full">
-        <div class="w-full grid grid-flow-col gap-4 items-center justify-end">
-            <a href="https://github.com/anon962/AmyBotV2-web" target="_blank">
+        <div class="w-full flex gap-1 items-center justify-end">
+            <a
+                href="https://github.com/anon962/AmyBotV2-web"
+                target="_blank"
+                class="btn btn-circle"
+            >
                 <GithubIcon width="24" height="24" variant={iconVariant} />
             </a>
 
-            <a href="https://discord.gg/7W6y7ezd" target="_blank">
+            <a href="https://discord.gg/7W6y7ezd" target="_blank" class="btn btn-circle">
                 <DiscordIcon width="24" height="24" variant={iconVariant} />
             </a>
         </div>
     </nav>
 </footer>
+
+<style lang="postcss">
+    .btn {
+        @apply h-10 w-10 p-0 min-h-0;
+    }
+</style>
